8d24b5ff80bdffda931b0bc2f38cd804876aaf16
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-02-23 09:21:24 -0500

    styled portfolio cards

--- src/components/PortCard.js
+++ src/components/PortCard.js
@@ -1,11 +1,13 @@
 import { Card } from "react-bootstrap";
 
-const PortCard = ({link, img, title, desc}) => {
+const PortCard = ({ link, img, title, desc }) => {
 
     return <Card as='a' href={link}>
-        <Card.Header>{title}</Card.Header>
-        <Card.Img src={img} alt='showcase of a website'/>
-        <Card.Text>{desc}</Card.Text>
+        <Card.Img src={img} alt='showcase of a website' />
+        <Card.ImgOverlay>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{desc}</Card.Text>
+        </Card.ImgOverlay>
     </Card>
 }
 
