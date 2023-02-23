2978107387b21cfc7f8d9e668e85d1f7ac4d576e
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-02-23 13:06:47 -0500

    wip adding contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,50 @@
+import { useFetcher } from "react-router-dom"
+
+export async function action({request, params}){
+    const formData = await request.formData()
+}
+
 const Contact = () => {
-    return <h1>Contact Form etc</h1>
+    const fetcher = useFetcher()
+
+    return <>
+        <h1>Contact</h1>
+        <fetcher.Form action="https://api.web3forms.com/submit" method="POST" >
+
+            {/* REQUIRED: Your Access key here. Don't worry this can be public */}
+            <input type="hidden" name="access_key" value="e3079f71-d6c1-4b9f-ad0a-4105c49b544a" />
+
+            {/* Optional: Subject an be prefilled using type="hidden"
+     or type="text" for normal user submitted input */}
+            <input type="hidden" name="subject" value="New Submission from Josef Kretz Web Developer" />
+
+            {/* Optional: From Name you want to see in the email
+     Default is "Notifications". you can overwrite here */}
+            <input type="hidden" name="from_name" value="Josef Kretz" />
+
+            {/* Optional: Default replyto will be "email" (if available)
+     you may overwrite replyto with different email here */}
+            <input type="hidden" name="replyto" value="" />
+
+            {/* Required: If submitting form without Javascript.
+     Default web3forms output is in JSON format. */}
+            {/* Not Required: If javascript, use "window.location.hash" instead */}
+            {/* <input type="hidden" name="redirect" value="https://web3forms.com/success" /> */}
+
+            {/* Optional: But Recommended: To Prevent SPAM Submission.
+     Make sure its hidden by default */}
+            <input type="checkbox" name="botcheck" className="hidden" style={{display: "none"}} />
+
+            {/* Custom Form Data: Form data you wish to receive in email. */}
+            <input type="email" name="email" required placeholder='E-mail' />
+            <input type="text" name="First Name" required placeholder='First name' />
+            <input type="tel" name="Phone Number" placeholder='Phone Number (optional)' />
+            <textarea name="message" required placeholder='Enter your message'></textarea>
+
+            <button type="submit">Submit Form</button>
+
+        </fetcher.Form>
+    </>
 }
 
 export default Contact
