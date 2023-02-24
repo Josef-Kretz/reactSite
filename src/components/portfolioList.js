f5a82b5b5b24157d05dd4a5688e267050ac7698f
Josef-Kretz <86017280+Josef-Kretz@users.noreply.github.com>
2023-02-24 12:37:19 -0500

    added more portfolio projects

--- src/components/portfolioList.js
+++ src/components/portfolioList.js
@@ -1,5 +1,17 @@
 const portfolioList = [
     {
+        title: 'AniSmash',
+        link: 'https://anismash.onrender.com',
+        img: require('../assets/portfolio/anismash.jpg'),
+        desc: 'MERN full stack app that allows you to check out trending anime and their trailers.'
+    },
+    {
+        title: 'Shop Math',
+        link: 'https://shopmath.netlify.com',
+        img: require('../assets/portfolio/shopmath.jpg'),
+        desc: 'React app that helps you practice common shop math, i.e. fractions!'
+    },
+    {
         title: 'Drumkit',
         link: 'drumkit.html',
         img: require('../assets/portfolio/drumkit.jpg'),
@@ -15,7 +27,7 @@ const portfolioList = [
         title: 'Horoscope Generator',
         link: 'horoscope.html',
         img: require('../assets/portfolio/horoscope.jpg'),
-        desc: 'Enter a birthdate and get a horoscope based on your star sign'
+        desc: 'Enter a birthdate and get a horoscope based on the star sign'
     }
 ]
 
